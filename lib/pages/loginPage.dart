bd38b047a8f1626e3226c8e57878afc5b73a56ec
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-11-21 21:15:17 -0300

    podjqwopdjqpwod

---
+++ lib/pages/loginPage.dart
@@ -0,0 +1,34 @@
+import 'package:app_eventos/pages/homePage.dart';
+import 'package:app_eventos/services/google-sign-in.dart';
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Iniciar Sesion'),
+                IconButton(
+                  icon: Icon(MdiIcons.google),
+                  onPressed: () async{
+                    await iniciarSesionConGoogle();
+                    
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/eventos_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class eventos_widget extends StatelessWidget {
+  const eventos_widget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
