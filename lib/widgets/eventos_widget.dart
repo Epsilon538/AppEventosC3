4a7467266ba5f6c1b389ab03b5ffe83e7eadb055
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-11-21 23:53:10 -0300

    wqdqwd

--- lib/widgets/eventos_widget.dart
+++ lib/widgets/eventos_widget.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
-class eventos_widget extends StatelessWidget {
-  const eventos_widget({super.key});
+class eventos_widget extends StatefulWidget {
+  const eventos_widget({
+    required this.nombre,
+    required this.fecha,
+    required this.hora,
+    required this.lugar,
+    required this.descripcion,
+    required this.tipo,
+    required this.likes,
+  });
 
+  final String nombre;
+  final DateTime fecha;
+  final String hora;
+  final String lugar;
+  final String descripcion;
+  final String tipo;
+  final int likes;
+
+  @override
+  State<eventos_widget> createState() => _eventos_widgetState();
+}
+
+class _eventos_widgetState extends State<eventos_widget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Column(
+        children: [
+          
+      ]),
     );
   }
 }
