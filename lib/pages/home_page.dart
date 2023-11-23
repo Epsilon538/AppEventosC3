6549fd493f6bc560b2ddf41f4fb99d19e0889d1d
Bastian <bastian.rojash@usm.cl>
2023-11-23 19:46:41 -0300

    Cambios aun mas importantes :(

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
-import 'package:app_eventos/pages/agregar_evento_page.dart';
 import 'package:app_eventos/pages/eventos_page.dart';
 import 'package:app_eventos/pages/finalizados_page.dart';
+import 'package:app_eventos/pages/login_page.dart';
 import 'package:app_eventos/pages/proximos_page.dart';
 import 'package:app_eventos/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -20,6 +20,40 @@ class _HomePageState extends State<HomePage> {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
+          actions: [
+            StreamBuilder(
+                stream: AuthService().usuario,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (FirebaseAuth.instance.currentUser?.displayName != null) {
+                    return PopupMenuButton(
+                      itemBuilder: (context) => [
+                        PopupMenuItem(
+                          child: Text('Cerrar sesión'),
+                          value: 'cerrar',
+                        )
+                      ],
+                      onSelected: (value) {
+                        FirebaseAuth.instance.signOut();
+                      },
+                    );
+                  } else {
+                    return PopupMenuButton(
+                      itemBuilder: (context) => [
+                        PopupMenuItem(
+                          child: Text('Iniciar sesión'),
+                          value: 'iniciar',
+                        )
+                      ],
+                      onSelected: (value) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LoginPage()));
+                      },
+                    );
+                  }
+                })
+          ],
           title: Text('Eventasos'),
           bottom: TabBar(tabs: [
             Tab(
