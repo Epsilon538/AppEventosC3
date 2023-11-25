2f630b9470fbfa25d1fe235b7ee687d53fe13547
Epsilon <134446210+Epsilon538@users.noreply.github.com>
2023-11-24 21:08:35 -0300

    Login modificado

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,18 @@
+import 'package:app_eventos/services/auth_service.dart';
 import 'package:app_eventos/services/google-sign-in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,14 +22,24 @@ class LoginPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
+                Container(
+                  child: Center(
+                    child: Image(image: NetworkImage(FirebaseAuth.instance.currentUser?.photoURL.toString() ?? '')),   
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(FirebaseAuth.instance.currentUser?.displayName != null ? 'Logued as ${FirebaseAuth.instance.currentUser?.displayName ?? 'Unknown'}': 'Not logued', style: TextStyle(fontSize: 15)),                   
+                SizedBox(height: 20),
                 Text('Iniciar Sesion'),
                 IconButton(
                   icon: Icon(MdiIcons.google),
                   onPressed: () async {
                     await iniciarSesionConGoogle();
-                    Navigator.pop(context);
+                    setState(() {
+                      
+                    });
                   },
                 ),
               ],
